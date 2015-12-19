5368e6a0badcfa4d9c5c1642911f3f57e1286b32
s1hsgw <shsgwixdjs@gmail.com>
2015-12-19 19:45:27 +0900

    Added link to TEDx Talks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,10 @@
-'use strict';
+$(document).ready(function($) {
+    var delay = 1000;
+    var header = $('.header-content-inner');
 
-$(document).ready(function() {
+    setTimeout(function() {
+        header.addClass('animated fadeIn').show();
+    }, delay);
 
-    $(".english").on('click', function() {
-        $('.eng').css('display', 'block');
-        $('.jp').css('display', 'none');
-    });
 
-    $(".japanese").on('click', function() {
-        $('.jp').css('display', 'block');
-        $('.eng').css('display', 'none');
-    });
-
-    $('.circle-icon-twitter').bind({
-        'touchstart': function(e) {
-            $(this).removeClass("tw-notouchstyle").addClass("tw-touchstyle");
-        },
-        'touchend': function(e) {
-            $(this).removeClass("tw-notouchstyle").addClass("tw-touchstyle");
-        }
-    });
-
-    $('.circle-icon-facebook').bind({
-            'touchstart': function(e) {
-                $(this).removeClass("fb-notouchstyle").addClass("fb-touchstyle");
-            },
-            'touchend': function(e) {
-                $(this).removeClass("fb-notouchstyle").addClass("fb-touchstyle");
-            }
-    });
 });
